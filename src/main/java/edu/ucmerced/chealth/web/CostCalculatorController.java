c49106732d5c7b7e74f5b18b64e6a996ae094bad
Yash Gupta <guptayash1216@gmail.com>
2024-03-18 23:39:06 +0530

    allowed cross origin for all sources

--- src/main/java/edu/ucmerced/chealth/web/CostCalculatorController.java
+++ src/main/java/edu/ucmerced/chealth/web/CostCalculatorController.java
@@ -4,6 +4,7 @@ import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -18,6 +19,7 @@ import edu.ucmerced.chealth.datasource.health.domain.ROICalculatorRequest;
 import edu.ucmerced.chealth.service.CostCalculatorService;
 import edu.ucmerced.chealth.service.ROICalculatorService;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/api")
 public class CostCalculatorController {    
